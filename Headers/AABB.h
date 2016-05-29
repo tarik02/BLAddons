62699832a94a31a077a35325aa10d2c8120decdb
Jose Vazquez <cabo.rambo@hotmail.com>
2016-05-28 22:36:46 -0500

    Added missing struct member to AABB class

--- Headers/AABB.h
+++ Headers/AABB.h
@@ -2,8 +2,10 @@
 
 struct Vec3;
 
+// Size: 28
 struct AABB {
-	float x1, y1, z1, x2, y2, z2; // 0-28
+	float x1, y1, z1, x2, y2, z2;	// 0-24
+	bool valid;						// 24-28
 	
 	AABB();
 	AABB(const Vec3&, float);
