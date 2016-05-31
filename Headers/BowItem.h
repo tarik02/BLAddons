79cd5f3184b5d7b5dba4399d894775d3d4d5fc3f
Jose Vazquez (Cabo Rambo) <jose-vm@users.noreply.github.com>
2016-05-30 19:09:53 -0500

    Added explanation

--- Headers/BowItem.h
+++ Headers/BowItem.h
@@ -12,6 +12,16 @@ public:
 	TextureUVCoordinateSet bow_pulling_2;	// 112-116; R8
 	char lifegoeson[20];			// 116-136
 	
+	/*
+	You may be thinking... why are those fillers with random names there?
+	MCPE compiler made this class'es TUVCS twenty-four bytes each, but
+	our addons compiler makes them four bytes long, so, to get access
+	to these struct members we need to fill in the blanks in the remaining
+	bytes in each BowItem class struct member.
+	I don't know how right or wrong this is, but it works for me, so I have
+	no problem with it.
+	*/
+	
 	//virtual ~BowItem();
 	virtual int getEnchantSlot() const;
 	virtual int getEnchantValue() const;
